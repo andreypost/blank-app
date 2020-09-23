b91ca9a3b002a975cf7ca4df8eaf3df76fb1b4d4
andrey post <postoliuk.av@gmail.com>
2020-09-23 16:13:12 +0300

    Updates

---
+++ precache-manifest.c80f5bea241c5306af445b09862918a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5691f9b62091f74f06b37486c984c6",
+    "url": "/blank-app/index.html"
+  },
+  {
+    "revision": "8ac2aba64d21c80b634d",
+    "url": "/blank-app/static/css/2.08c04568.chunk.css"
+  },
+  {
+    "revision": "f1f1bd962f6676a81914",
+    "url": "/blank-app/static/css/main.2d9abd44.chunk.css"
+  },
+  {
+    "revision": "8ac2aba64d21c80b634d",
+    "url": "/blank-app/static/js/2.874cbcdb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blank-app/static/js/2.874cbcdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f1bd962f6676a81914",
+    "url": "/blank-app/static/js/main.efe58390.chunk.js"
+  },
+  {
+    "revision": "559ae6244233427afe4a",
+    "url": "/blank-app/static/js/runtime-main.9bbce275.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/blank-app/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/blank-app/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/blank-app/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
