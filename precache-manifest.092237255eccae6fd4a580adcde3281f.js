bb83c48382f044475d21b356ac465f5dee835cc4
andrey post <postoliuk.av@gmail.com>
2020-09-23 16:21:53 +0300

    Updates

--- precache-manifest.092237255eccae6fd4a580adcde3281f.js
+++ precache-manifest.092237255eccae6fd4a580adcde3281f.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a5691f9b62091f74f06b37486c984c6",
+    "revision": "e597b9a36823e2da8564c5072766b254",
     "url": "/blank-app/index.html"
   },
   {
-    "revision": "8ac2aba64d21c80b634d",
+    "revision": "f54b8e6e31f253ee7f83",
     "url": "/blank-app/static/css/2.08c04568.chunk.css"
   },
   {
-    "revision": "f1f1bd962f6676a81914",
+    "revision": "858ba367a5de4c45183c",
     "url": "/blank-app/static/css/main.2d9abd44.chunk.css"
   },
   {
-    "revision": "8ac2aba64d21c80b634d",
-    "url": "/blank-app/static/js/2.874cbcdb.chunk.js"
+    "revision": "f54b8e6e31f253ee7f83",
+    "url": "/blank-app/static/js/2.8663b771.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/blank-app/static/js/2.874cbcdb.chunk.js.LICENSE.txt"
+    "url": "/blank-app/static/js/2.8663b771.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f1f1bd962f6676a81914",
-    "url": "/blank-app/static/js/main.efe58390.chunk.js"
+    "revision": "858ba367a5de4c45183c",
+    "url": "/blank-app/static/js/main.9dc7d4a5.chunk.js"
   },
   {
-    "revision": "559ae6244233427afe4a",
-    "url": "/blank-app/static/js/runtime-main.9bbce275.js"
+    "revision": "74fd147045a9dfd5fb93",
+    "url": "/blank-app/static/js/runtime-main.179a8de0.js"
   },
   {
     "revision": "f7eab148df329432a159f8a1cccb102f",
