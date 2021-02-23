4abb84824449a3cb4bb781f0b9430f4e72d91406
andrey post <postoliuk.av@gmail.com>
2021-02-23 09:20:45 +0200

    Updates

---
+++ precache-manifest.b498641016600af161a6a1de774a71bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d768382ec6df5a7f1a8535e78510ce20",
+    "url": "/contacts-table/index.html"
+  },
+  {
+    "revision": "ecd2484753d92c931b27",
+    "url": "/contacts-table/static/css/2.08c04568.chunk.css"
+  },
+  {
+    "revision": "89b4a79b69dfa265271f",
+    "url": "/contacts-table/static/css/main.88ce771f.chunk.css"
+  },
+  {
+    "revision": "ecd2484753d92c931b27",
+    "url": "/contacts-table/static/js/2.c18fb40a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contacts-table/static/js/2.c18fb40a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b4a79b69dfa265271f",
+    "url": "/contacts-table/static/js/main.9bf04875.chunk.js"
+  },
+  {
+    "revision": "aa9d40fd6899acce043f",
+    "url": "/contacts-table/static/js/runtime-main.741c002a.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/contacts-table/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/contacts-table/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/contacts-table/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blank-app/precache-manifest.0e18267bff45f1b07d2a00bd8d9f82b7.js"
+  "/contacts-table/precache-manifest.b498641016600af161a6a1de774a71bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blank-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contacts-table/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
